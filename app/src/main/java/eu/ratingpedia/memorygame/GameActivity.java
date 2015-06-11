0c764f30740b3b068999331ab6a85793cede95a1
marcinwal <marcin.walendzik@gmail.com>
2015-06-11 15:22:33 +0100

    Loading sounds

--- app/src/main/java/eu/ratingpedia/memorygame/GameActivity.java
+++ app/src/main/java/eu/ratingpedia/memorygame/GameActivity.java
@@ -1,12 +1,17 @@
 package eu.ratingpedia.memorygame;
 
 import android.app.Activity;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.logging.Handler;
 
 
@@ -47,6 +52,26 @@ public class GameActivity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC,0);
+        try{
+            AssetManager assetManager = getAssets();
+            AssetFileDescriptor descriptor;
+
+            descriptor = assetManager.openFd("sample1.ogg");
+            sample1 = soundPool.load(descriptor,0);
+
+            descriptor = assetManager.openFd("sample2.ogg");
+            sample2 = soundPool.load(descriptor,0);
+
+            descriptor = assetManager.openFd("sample3.ogg");
+            sample3 = soundPool.load(descriptor,0);
+
+            descriptor = assetManager.openFd("sample4.ogg");
+            sample4 = soundPool.load(descriptor,0);
+
+        }catch(IOException e){
+            Toast.makeText(getApplicationContext(),"COULD NOT LOAD FILES",Toast.LENGTH_LONG).show();
+        }
 
     }
 
